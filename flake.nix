15cf37d350f82fd5fd1d6344306ed4e320ff68be
Ildar Nasyrov <iledarnp@gmail.com>
2024-06-28 13:43:00 +0800

    use dedicated config for scnsoft notebook

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       scnsoft = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
+          ./scnsoft-configuration.nix
 
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
