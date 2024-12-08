b5773ff006b628e4e2155ad7519feb2dc3144829
Ildar Nasyrov <iledarnp@gmail.com>
2024-12-08 20:06:15 +0800

    add google-drive-ocamlfuse

--- home.nix
+++ home.nix
@@ -73,7 +73,36 @@
     luajitPackages.lua-lsp
     lua-language-server
     gedit
+    google-drive-ocamlfuse
+    fuse
   ];
+
+  systemd.user.services = {
+    google-drive-mount = {
+      Unit = {
+        Description = "Mount Google Drive";
+        After = [ "network-online.target" ];
+        Wants = [ "network-online.target" ];
+      };
+
+      Service = {
+        Type = "forking";
+        ExecStart = "${pkgs.google-drive-ocamlfuse}/bin/google-drive-ocamlfuse %h/GoogleDrive";
+        ExecStop = "${pkgs.fuse}/bin/fusermount -u %h/GoogleDrive";
+        Restart = "on-failure";
+        RestartSec = "30s";
+      };
+
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
+  };
+
+  # home.file.googleDrive = {
+  #   target = "GoogleDrive";
+  #   directory = true;
+  # };
 
   programs.bash = {
     enable = true;
@@ -97,51 +126,51 @@
   programs.vscode = {
     enable = true;
     extensions = with pkgs.vscode-extensions;
-      [
-        brettm12345.nixfmt-vscode
-        mkhl.direnv
-        dracula-theme.theme-dracula
-        vscodevim.vim
-        yzhang.markdown-all-in-one
-        bbenoist.nix
-        ms-azuretools.vscode-docker
-        ms-python.black-formatter
-        ms-python.python
-        ms-python.isort
-        xyz.local-history
-      ]
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "vsc-invoke";
-          publisher = "dchanco";
-          version = "0.0.12";
-          sha256 = "sha256-+YpNftJ9qIfZqGQXZAb4+E0V8/aa8zWTjRSphddQw68=";
-        }
-        {
-          name = "tasks";
-          publisher = "actboy168";
-          version = "0.16.0";
-          sha256 = "sha256-btYWdOuxqSBclBHKyICo2yNmTjB7tOpiKNFNASPgihU=";
-        }
-        {
-          name = "aws-toolkit-vscode";
-          publisher = "AmazonWebServices";
-          version = "3.12.0";
-          sha256 = "sha256-110Hn80Nll8vO8EgeQ7coKyspjUR6TyyCTmdIdkzHZ4=";
-        }
-        {
-          name = "amazon-q-vscode";
-          publisher = "AmazonWebServices";
-          version = "1.11.0";
-          sha256 = "sha256-5Op1ivgeVzPvIuT5qeY67Oe8xm1wWWIaSgp4jzDBau0=";
-        }
-        {
-          name = "prettier-vscode";
-          publisher = "esbenp";
-          version = "10.4.0";
-          sha256 = "sha256-8+90cZpqyH+wBgPFaX5GaU6E02yBWUoB+T9C2z2Ix8c=";
-        }
-      ];
+    [
+      brettm12345.nixfmt-vscode
+      mkhl.direnv
+      dracula-theme.theme-dracula
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+      bbenoist.nix
+      ms-azuretools.vscode-docker
+      ms-python.black-formatter
+      ms-python.python
+      ms-python.isort
+      xyz.local-history
+    ]
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "vsc-invoke";
+        publisher = "dchanco";
+        version = "0.0.12";
+        sha256 = "sha256-+YpNftJ9qIfZqGQXZAb4+E0V8/aa8zWTjRSphddQw68=";
+      }
+      {
+        name = "tasks";
+        publisher = "actboy168";
+        version = "0.16.0";
+        sha256 = "sha256-btYWdOuxqSBclBHKyICo2yNmTjB7tOpiKNFNASPgihU=";
+      }
+      {
+        name = "aws-toolkit-vscode";
+        publisher = "AmazonWebServices";
+        version = "3.12.0";
+        sha256 = "sha256-110Hn80Nll8vO8EgeQ7coKyspjUR6TyyCTmdIdkzHZ4=";
+      }
+      {
+        name = "amazon-q-vscode";
+        publisher = "AmazonWebServices";
+        version = "1.11.0";
+        sha256 = "sha256-5Op1ivgeVzPvIuT5qeY67Oe8xm1wWWIaSgp4jzDBau0=";
+      }
+      {
+        name = "prettier-vscode";
+        publisher = "esbenp";
+        version = "10.4.0";
+        sha256 = "sha256-8+90cZpqyH+wBgPFaX5GaU6E02yBWUoB+T9C2z2Ix8c=";
+      }
+    ];
   };
 
   programs.tmux = {
