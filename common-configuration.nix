9346ac249021aa4d7aa0b57e23e95c8ab3501c92
Ildar Nasyrov <iledarnp@gmail.com>
2024-12-21 18:36:19 +0800

    add hosts for local odoo

--- common-configuration.nix
+++ common-configuration.nix
@@ -29,6 +29,8 @@
     127.0.0.1 doodba16
     127.0.0.1 doodba17
     139.162.11.95 usdtest12.kaertech.com
+    127.0.0.1 erp1215.localhost
+    127.0.0.1 erp1202.localhost
   '';
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
