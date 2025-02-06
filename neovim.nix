51f90974b702b624792fb6a4dd81e8300e2f755a
Ildar Nasyrov <iledarnp@gmail.com>
2025-02-07 01:16:35 +0800

    add lazy-nvim

--- neovim.nix
+++ neovim.nix
@@ -14,5 +14,21 @@
     extraPackages = [
       pkgs.pyright
     ];
+    plugins = [ pkgs.vimPlugins.lazy-nvim ];
+    extraLuaConfig =
+      # lua
+      ''
+      require("lazy").setup({
+        -- disable all update / install features
+        -- this is handled by nix
+        rocks = { enabled = false },
+        pkg = { enabled = false },
+        install = { missing = false },
+        change_detection = { enabled = false },
+        spec = {
+          -- TODO
+        },
+      })
+      '';
   };
 }
