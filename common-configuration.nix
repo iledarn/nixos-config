a3c60f10421a44f94c0c68a7b4231b556db70657
Ildar Nasyrov <iledarnp@gmail.com>
2025-02-16 13:37:30 +0800

    add /etc/hosts foor erp2025_02_10

--- common-configuration.nix
+++ common-configuration.nix
@@ -38,6 +38,7 @@
     127.0.0.1 erp2025_01_26.localhost
     127.0.0.1 erp2025_02_07.localhost
     127.0.0.1 erp2025_02_05.localhost
+    127.0.0.1 erp2025_02_10.localhost
   '';
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
