67355c91ca4509512b0a798ba3e9c5a98e17b397
Ildar Nasyrov <iledarnp@gmail.com>
2026-01-23 10:41:25 +0800

    add resume to fzf-lua live grep

--- nvim/plugin/fzf-lua.lua
+++ nvim/plugin/fzf-lua.lua
@@ -3,6 +3,7 @@ vim.keymap.set("n", "<leader>tt", "<cmd>lua require('fzf-lua').tabs()<CR>", { si
 vim.keymap.set("n", "<leader>lg", "<cmd>lua require('fzf-lua').live_grep_glob()<CR>", { silent = true })
 vim.keymap.set("n", "<leader>lG", "<cmd>lua require('fzf-lua').fzf_live('rg --no-ignore-vcs --column --line-number --no-heading --color=always --smart-case', { previewer = 'builtin' })<CR>", { silent = true })
 vim.keymap.set("n", "<leader>Lg", "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>lr", "<cmd>lua require('fzf-lua').live_grep_glob({ resume = true })<CR>", { silent = true })
 vim.keymap.set("n", "<leader>gg", "<cmd>lua require('fzf-lua').grep()<CR>", { silent = true })
 vim.keymap.set("n", "<leader>gl", "<cmd>lua require('fzf-lua').grep_last()<CR>", { silent = true })
 vim.keymap.set("n", "<leader>gp", "<cmd>lua require('fzf-lua').grep_project()<CR>", { silent = true })
