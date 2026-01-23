23d1ff0b6d1acfe7762e6afce358ac97cb0a4e33
Ildar Nasyrov <iledarnp@gmail.com>
2026-01-23 14:19:38 +0800

    add hotkey for dapui hover

--- nvim/plugin/nvim-dap-ui.lua
+++ nvim/plugin/nvim-dap-ui.lua
@@ -2,5 +2,16 @@ require("dapui").setup()
 vim.keymap.set("n", "\\do", "<cmd>lua require('dapui').open()<CR>", { silent = true })
 vim.keymap.set("n", "\\dc", "<cmd>lua require('dapui').close()<CR>", { silent = true })
 vim.keymap.set("n", "\\dh", function()
-  require("dapui").eval(nil, { width = 80, height = 10, enter = true })
+  require("dapui").eval(nil, {
+    width = math.floor(vim.o.columns * 0.8),
+    height = 10,
+    enter = true,
+  })
+end, { silent = true })
+vim.keymap.set("n", "<leader>dh", function()
+  require("dapui").eval(nil, {
+    width = math.floor(vim.o.columns * 0.8),
+    height = 10,
+    enter = true,
+  })
 end, { silent = true })
