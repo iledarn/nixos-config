52ef7a5844284c17cccbe5a4058c33713b2a4a69
Ildar Nasyrov <iledarnp@gmail.com>
2026-02-01 20:09:53 +0800

    fix nvim-lspconfig.lua - resolve executables with vim.fn.exepath instead - because nix syntax is not working in lua files

--- nvim/plugin/nvim-lspconfig.lua
+++ nvim/plugin/nvim-lspconfig.lua
@@ -1,13 +1,21 @@
 -- Nil language server setup
 local lspconfig = require('lspconfig')
--- Get the Nil binary path from the Nix store
-local nil_bin = "${pkgs.nil}/bin/nil"
+
+local function resolve_bin(name, fallback)
+  local exe = vim.fn.exepath(name)
+  if exe ~= "" then
+    return exe
+  end
+  return fallback or name
+end
+
+local nil_bin = resolve_bin("nil")
 lspconfig.nil_ls.setup {
   cmd = { nil_bin },
   on_attach = on_attach,
   capabilities = capabilities,
 }
-local lua_lsp_bin = "${pkgs.lua-language-server}/bin/lua-language-server"
+local lua_lsp_bin = resolve_bin("lua-language-server")
 lspconfig.lua_ls.setup {
   cmd = { lua_lsp_bin, "-E", "-e", "LANG=en" },
   capabilities = capabilities,
